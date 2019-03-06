f86ac46d5cad80a157fa045c06dc27db433a1520
Alexander Raschid <alexraschid1337@gmail.com>
2019-03-05 23:04:18 -0500

    Added navbar, working on styling rn

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import style from 'bootstrap/dist/css/bootstrap.css';
+import {
+  Container, Row, Col,
+  Navbar, Nav, NavDropdown,
+  Form, FormControl, Button
+} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
 
       <Container>
-        <Row>
-          <Col>1of3</Col>
-          <Col>2of3</Col>
-          <Col>3of3</Col>
-        </Row>
+        <Container>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+
       </Container>
     );
   }
