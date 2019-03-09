bcafaba85e410afb03ecf119992e34af5b6c3bcd
Alex Raschid <alexraschid1337@gmail.com>
2019-03-09 11:23:30 -0500

    Added two buttons and changed sample bio text

--- src/my-components/bio-components/MyRow.js
+++ src/my-components/bio-components/MyRow.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Row, Col, Image, Button } from 'react-bootstrap';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 export class MyRow extends React.Component{
     render(){
         return(
             <Row>
-                <Col md={3} sm={3} xs={12}>
+                <Col md={3} sm={3} xs={5}>
                 <Image fluid src={this.props.default_img} roundedCircle />
+                    <Row>
+                       
+                    </Row>  
                 </Col>
-                <Col md={9} sm={9}>
-                <p>
-                    Blah blah blah mock up text for this bio or something. This is a modified jumbotron that occupies the entire horizontal space of
-                    its parent.
-                </p>
+                <Col md={9} sm={9} xs={7}>
+                    <Row>
+                        <p>
+                            Hey im Alex and This is my portfolio website
+                        </p>
+                    </Row>
+                    <Row>
+                        <Button href="www.github.com/alexraschid">Link</Button>
+                        <Button>Resume</Button>
+                    </Row>
+                
                 </Col>
             </Row>
 
