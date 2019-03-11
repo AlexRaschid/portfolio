4bdb4e3085705b0b5badebc956827e7f1ac307fc
AlexRaschid <alexraschid1337@gmail.com>
2019-03-11 12:02:07 -0400

    added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+  var config = {
+    apiKey: "AIzaSyB0nCVL6CqZ7F2SrGPrcQ5vztGRubxXNjI",
+    authDomain: "portfolio-d741a.firebaseapp.com",
+    databaseURL: "https://portfolio-d741a.firebaseio.com",
+    projectId: "portfolio-d741a",
+    storageBucket: "portfolio-d741a.appspot.com",
+    messagingSenderId: "32459784399"
+  };
+firebase.initializeApp(config);
+export default firebase;
+//https://css-tricks.com/intro-firebase-react/
