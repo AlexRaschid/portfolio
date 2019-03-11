ea11d5bafa23474a26f7b0d00eb95071c5721bc5
AlexRaschid <alexraschid1337@gmail.com>
2019-03-11 14:08:17 -0400

    this.props.title puts the title of the project from the firebaseDB

--- src/my-components/card-components/CardContent.js
+++ src/my-components/card-components/CardContent.js
@@ -9,18 +9,24 @@ import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 //https://blog.bitsrc.io/building-a-todo-app-in-react-with-firebase-and-redux-ba3ab53a671b
 
 export class CardContent extends React.Component{
+  constructor(props){
+    super(props);
+    //console.log(this.state.projects);
+  }
 
     render(){
+      console.log(this.props);
+
         return(
         <Card>
             <Card.Img variant="top" src={this.props.default_img}/>
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>{this.props.title}</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Button variant="primary">{this.props.title}</Button>
             </Card.Body>
         </Card>        
 
