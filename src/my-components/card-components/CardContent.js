6efc09a1668e6ef34f257825f4dd6c315391c7a8
AlexRaschid <alexraschid1337@gmail.com>
2019-03-18 22:52:18 -0400

    Finally got Images to load, Cleaned up Apis, removed APIS.js, Cleaned up some Comments and spacing, Added firebase storage reference to get images with the getImages() method, componentWillMount() calls this.getImage(), this.getImage() finds the image in the DB and sets this.state.image to the url

--- src/my-components/card-components/CardContent.js
+++ src/my-components/card-components/CardContent.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { Container, Button,
-         CardColumns, Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import firebase from '../../firebase.js';
-import APIS from '../APIS.js';
 
-
-
-//https://blog.bitsrc.io/building-a-todo-app-in-react-with-firebase-and-redux-ba3ab53a671b
+const storage = firebase.storage().ref("images/projects/")
 
 export class CardContent extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      
+
     }
+  }
+
+  getImage (image) {
+    storage.child(`${image}.jpg`).getDownloadURL().then((url) => {
+    console.log(url) 
+    this.state.image = url;
+    console.log(this.state.image);
+     this.setState(this.state);
+   }).catch(e=> {})
+   ;
+ }
+
+  componentWillMount(){
+    this.getImage(this.props.pathNames[this.props.pathIndex])
   }
   
 
     render(){
-      console.log(this.props);
-
+      console.log(this.state.image);
         return(
           <Card>
-              <Card.Img variant="top" src={APIS.getImage(this.props.pathName[0])}/>
+              <Card.Img variant="top" src={this.state.image}/>
               <Card.Body>
                 <Card.Title>{this.props.project.title}</Card.Title>
                 <Card.Text>
@@ -32,10 +41,5 @@ export class CardContent extends React.Component{
               </Card.Body>
           </Card>        
         );
-
     }
-
-
-
-
 }
