0a6f3f9f0103674150ba504dc8cb9844c3eca8d9
AlexRaschid <alexraschid1337@gmail.com>
2019-04-28 10:37:08 -0400

    Placeholder File
    Here for now; Features to come

---
+++ src/my-components/MyContact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container,
+         CardColumns } from 'react-bootstrap';
+import {CardContent} from './portfolio-components/CardContent';
+
+
+export class MyContact extends React.Component{
+    render(){
+        return(
+            <Container>
+                <h1>Contact Placeholder</h1>
+            </Container>
+        );
+    }
+}
---
+++ src/my-components/MyResumeCV.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container,
+         CardColumns } from 'react-bootstrap';
+import {CardContent} from './portfolio-components/CardContent';
+
+
+export class MyResumeCV extends React.Component{
+    render(){
+        return(
+            <Container>
+                <h1>ResumeCV Placeholder</h1>
+            </Container>
+        );
+    }
+}
