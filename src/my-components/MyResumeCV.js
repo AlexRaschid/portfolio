3bd8919c52284356c6d368db1a17a3cd1aec8964
AlexRaschid <alexraschid1337@gmail.com>
2019-04-29 03:35:57 -0400

    react-pdf working; CORS proxy needed
    I got the component to pull the pdf file from firebase and to potentially display it on screen from the Document component, however i might need to use a CORS proxy to actually pull it up on screen.

--- src/my-components/MyResumeCV.js
+++ src/my-components/MyResumeCV.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import {Container, Row} from 'react-bootstrap';
-import { Document } from 'react-pdf'
+import { Document, Page } from 'react-pdf';
 
+import firebase from '../firebase.js';
+
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 export class MyResumeCV extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            numPages: null,
+            pageNumber: 1,
+            
+        }
+        console.log(this.state);
+        this.onDocumentLoad = this.onDocumentLoad.bind(this)
+    }
+
+    onDocumentLoad ({ numPages }) {
+        this.setState({ numPages })
+      }
+    
+    componentWillMount(){
+        //ToDo: Add CORS proxy to fiew the pdf file 
+        firebase.storage().ref("pdfs/")//gets pdf from firebase
+                          .child(`MyResume.pdf`)
+                          .getDownloadURL()
+                          .then((url) => {
+            console.log(url);
+            this.state.MyResume = url;
+            this.setState(this.state);
+
+        });
+    }
+    
     render(){
         return(
             <Container>
-                <h1>ResumeCV Placeholder</h1>
+                <Row>
+                    <Document file={this.state.MyResume}
+                              onDocumentLoad={this.onDocumentLoad}>
+                        <Page pageNumber={1} />
+                    
+                    </Document>
+                </Row>
             </Container>
         );
     }
