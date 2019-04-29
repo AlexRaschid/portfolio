9148f07b84c24a04b01bf5dea7fc15929a4f1126
AlexRaschid <alexraschid1337@gmail.com>
2019-04-29 01:01:32 -0400

    Code Cleanup
    Made the Columns more distinct visually and Added them to be on seperate lines when phone screen

--- src/my-components/MyBio.js
+++ src/my-components/MyBio.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {Container, Jumbotron} from 'react-bootstrap';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import {MyRow} from './bio-components/MyRow';
-
+import { Row, Col, Image, Button } from 'react-bootstrap';
 export class MyBio extends React.Component{
     render(){
+        //ToDo: Replace this.props.default_img with actual main picture
+        //ToDo: Feed Bio in from db?
         return(
             <Jumbotron id="my_bio">
               <Container>
-                
-              <MyRow default_img={this.props.default_img}/>             
-                  
+                <Row>
+                    <Col md={3} sm={3} xs={12}>
+                        <Image fluid src="https://www.petmd.com/sites/default/files/small-kitten-walking-towards_127900829_0.jpg" roundedCircle />
+                    </Col>
+                    <Col md={9} sm={9} xs={12}>
+                        <Row>
+                            <p id="myBio">
+                                Hello! My name is Alexander Raschid and this is my portfolio webpage.
+                                Ive been coding for 4 years and im currently  in my Freshmen Year at CUNY Brooklyn College
+                            </p>
+                        </Row>
+                    </Col>
+                </Row>
               </Container>
             </Jumbotron>
 
